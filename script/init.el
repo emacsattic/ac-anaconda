58214959eccb5728f8578ffd444c14c9f152f803
Malyshev Artem <proofit404@gmail.com>
2015-01-12 21:32:55 +0300

    Remove redundant anaconda-mode-debug.

--- script/init.el
+++ script/init.el
@@ -10,17 +10,9 @@
   (cask-initialize source-directory)
   (add-to-list 'load-path source-directory))
 
-;; Anaconda settings.
-
-(setq anaconda-mode-debug t)
-
-;; Auto complete settings.
-
 (require 'ac-anaconda)
 
 (add-hook 'python-mode-hook 'ac-anaconda-setup)
-
-;; Pyenv settings.
 
 (pyenv-mode)
 
